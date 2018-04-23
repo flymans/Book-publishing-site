20fb757fe84e158c85446d9a1d7d2ec8126b4f02
Alexander Panteleev <746590@mail.ru>
2018-04-23 23:12:10 +0300

    Added database output with style (fixed books.html to books.php
    Remain pagination and filters.

--- books.php
+++ books.php
@@ -6,21 +6,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <title>О нас</title>
-  <style type="text/css">
-    div {
-      font-size: x-large;
-    }
-  </style>
+  <title>Наши книги</title>
 </head>
-
 <body class="main-background">
+  <?php
+    include 'database.php';
+  ?>
   <header class="main-header container-fluid">
     <nav class="navbar navbar-light row justify-content-center" style="background-color: #e3f2fd;">
       <ul class="row nav">
         <li class="nav-item"><a class="nav-link" href="index.html ">Главная</a></li>
-        <li class="nav-item"><a class="nav-link active" href="#">О нас</a></li>
-        <li class="nav-item"><a class="nav-link" href="books.php">Наши книги</a></li>
+        <li class="nav-item"><a class="nav-link" href="about-us.html">О нас</a></li>
+        <li class="nav-item"><a class="nav-link active" href="#">Наши книги</a></li>
         <li class="nav-item"><a class="nav-link navbar-brand" href="index.html"><img src="img/bureau-logo.png" width=100;
                  height=50 alt="Логотип книжного магазина Бюро"></a></li>
         <li class="nav-item"><a class="nav-link" href="for_authors.html">Авторам</a></li>
@@ -29,22 +26,38 @@
     </nav>
   </header>
   <main class="container">
-    <div class="row">
-      <div class="col bg-light text-dark">
-        <img src="img/bureau-logo.png" height="375" width="500" class="rounded float-right border border-white" alt="Логотип издательства" style="margin:20px 0">
-        <img src="img/publishing.jpg" class="rounded float-right border border-white" alt="Картинка с книгами">
-      </div>
-      <div class="col bg-light text-dark">
-        <p><b>Издательство "Бюро"</b> сможет издать Вашу книгу очень быстро и качественно! Минимальный срок издания 10 дней, средний – 1 месяц. Выполняем весь спектр издательских и печатных работ. Печатаем только на безупречно белой бумаге. Используем только
-          профессиональное оборудование. Никаких ксероксов и ризографов!</p>
-        <h2>Почему мы?</h2>
-        <ul>
-          <li>За 20 лет работы мы издали сотни книг – мы знаем об издании книг всё!</li>
-          <li>Если Вы сильно заняты на своей работе, сотрудничая с нами, Вам не понадобится ничего, кроме телефона и электронной почты, чтобы получить свою книгу.</li>
-          <li>Над срочными заказами мы готовы работать без выходных.</li>
-          <li>Наши заказчики возвращаются к нам снова и снова.</li>
-        </ul>
-      </div>
+    <div class="col bg-light text-dark">
+      <h2>Список книг</h2>
+          <?php
+            $query = "SELECT * FROM books";
+            $result = mysql_query($query) or die(mysql_error());
+            while ($row = mysql_fetch_array($result))
+            {
+              echo '<div class="row">';
+              echo '<div class="col-md-2 text-center">';
+              echo '<img src="'.$row[6].'">';
+              echo '</div>';
+              echo '<div class="col-md-10">';
+              echo '<h5>'.$row[1].'</h5>';
+              echo '<p>
+                        <span style="color:#50C1E9; font-size:14px">'.$row[2].'</span><br><span>'.$row[4].' год • '.$row[3].' страниц • '.$row[8].'</span><br>
+                          <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#'.+$row[0].'" aria-expanded="false" aria-controls="collapseExample">
+                          Описание
+                          </button>
+                          <a href="'.$row[5].'/read">Пролистать</a>
+                    </p>
+                    <div class="collapse" id="'.+$row[0].'">
+                    <div class="card card-body">
+                      '.$row[7].'
+                    </div>
+                  </div>
+                    ';
+
+              echo '</div>';
+              echo '</div>';
+            }
+          ?>
+    </div>
   </main>
   <footer class="navbar bottom navbar-light bg-light" style="color:black">
     <p class="footer-contacts">
---
+++ database.php
@@ -0,0 +1,5 @@
+<?php
+$cnct = mysql_connect("localhost", "root", "");
+$dbselect = mysql_select_db("books");
+mysql_set_charset("utf8");
+?>

8d9963f0fd2960b549e96b1c200325d40e7ce47f
Alexander Panteleev <746590@mail.ru>
2018-05-01 17:51:28 +0300

    Changed connection info for db
    Uploading to http://s92612t7.beget.tech/

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-$cnct = mysql_connect("localhost", "root", "");
-$dbselect = mysql_select_db("books");
+$cnct = mysql_connect("localhost", "s92612t7_books", "123456");
+$dbselect = mysql_select_db("s92612t7_books");
 mysql_set_charset("utf8");
 error_reporting(E_ALL & ~E_NOTICE);
 ?>
